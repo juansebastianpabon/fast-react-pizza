5e3b5a30b565b76f82de3d5a187971ea3463775f
Juan Sebastian Pabon Mojica <nova159712@gmail.com>
2025-10-01 15:51:30 -0500

    Creacion de la estructura del proyecto usando VITE y aplicando una estructura de carpetas por FEATURE.Tambien establece las rutas y crea el LAYOUT

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home.jsx";
+import Menu from "./features/menu/Menu.jsx";
+import Cart from "./features/cart/Cart.jsx";
+import CreateOrder from "./features/order/CreateOrder.jsx";
+import Order from "./features/order/Order.jsx";
+import AppLayout from "./ui/AppLayout.jsx";
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:id", element: <Order /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
