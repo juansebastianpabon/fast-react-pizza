7b2f6594e57a34eb76f7a96a04c1c8e32d12efb3
Juan Sebastian Pabon Mojica <nova159712@gmail.com>
2025-10-01 18:15:47 -0500

    Añade el fetch de la data usando LOADERS provistos por react-router, también añade el manejo de errores usando la propiedad errorElement

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Error from "./ui/Error.jsx";
 import Home from "./ui/Home.jsx";
-import Menu from "./features/menu/Menu.jsx";
+import Menu, { loader as MenuLoader } from "./features/menu/Menu.jsx";
 import Cart from "./features/cart/Cart.jsx";
 import CreateOrder from "./features/order/CreateOrder.jsx";
 import Order from "./features/order/Order.jsx";
@@ -8,6 +9,7 @@ import AppLayout from "./ui/AppLayout.jsx";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -17,6 +19,8 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
+        loader: MenuLoader,
+        errorElement: <Error />,
       },
 
       { path: "/cart", element: <Cart /> },
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,12 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
+import Loader from "./Loader";
 import Header from "./Header";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
   return (
-    <div>
+    <div className='layout'>
       <Header />
-
+      {isLoading && <Loader />}
       <main>
         <h1>Content </h1>
         <Outlet />
